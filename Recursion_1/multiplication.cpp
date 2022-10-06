a2ee31c8ff9bd6017a609a921eac44ef27343217
Faraz-mobin17 <faraz.mobin17@gmail.com>
2022-10-06 18:25:58 +0530

    added multiplication file to recursion folder

---
+++ Recursion_1/multiplication.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int multiplyNumbers(int m, int n) {
+    // Write your code here
+	if (n == 0) return 0;
+    int ans = multiplyNumbers(m,n - 1);
+    return (ans + m);
+}
+
+int main() {
+    int m, n;
+    cin >> m >> n;
+    cout << multiplyNumbers(m, n) << endl;
+}
