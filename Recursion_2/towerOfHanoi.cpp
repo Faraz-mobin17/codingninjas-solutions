362a4906d171ce78fca52b0ea5f7186422a9ac5a
Faraz-mobin17 <faraz.mobin17@gmail.com>
2022-10-22 20:26:50 +0530

    removed sample.c and added code in towerOfHanoi

--- Recursion_2/towerOfHanoi.cpp
+++ Recursion_2/towerOfHanoi.cpp
@@ -3,6 +3,12 @@ using namespace std;
 void towerOfHanoi(int n, char source, char auxiliary, char destination)
 {
     // Write your code here
+    if (n == 0)
+        return;
+    // recursive call
+    towerOfHanoi(n - 1, source, destination, auxiliary);
+    cout << source << " " << destination << endl;
+    towerOfHanoi(n - 1, auxiliary, source, destination);
 }
 
 int main()
