77ebe8786eca69a3b826c3225383cadf145dcb49
Faraz-mobin17 <faraz.mobin17@gmail.com>
2022-12-05 00:08:47 +0530

    added driver code in bubbleSortLL and swapTwoNodeLL

---
+++ LinkedList_2/swapTwoNodeLL.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include "Node.cpp"
+using namespace std;
+
+Node *swapNodes(Node *head, int i, int j)
+{
+    // Write your code here
+}
+
+Node *takeinput()
+{
+    int data;
+    cin >> data;
+    Node *head = NULL, *tail = NULL;
+    while (data != -1)
+    {
+        Node *newnode = new Node(data);
+        if (head == NULL)
+        {
+            head = newnode;
+            tail = newnode;
+        }
+        else
+        {
+            tail->next = newnode;
+            tail = newnode;
+        }
+        cin >> data;
+    }
+    return head;
+}
+
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int i, j;
+        Node *head = takeinput();
+        cin >> i;
+        cin >> j;
+        head = swapNodes(head, i, j);
+        print(head);
+    }
+    return 0;
+}
