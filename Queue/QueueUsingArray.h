40f1ae7e8e0726f09bd852c836d1806be2c8e20e
Faraz-mobin17 <faraz.mobin17@gmail.com>
2022-12-16 23:45:12 +0530

    added Queue folder and QueueUsingArray file

---
+++ Queue/QueueUsingArray.h
@@ -0,0 +1,70 @@
+template <typename T>
+class QueueUsingArray
+{
+	T *data;
+	int nextIndex;
+	int size;
+	int firstIndex;
+	int capacity;
+
+public:
+	QueueUsingArray(int s)
+	{
+		data = new T[s];
+		nextIndex = 0;
+		firstIndex = -1;
+		size = 0;
+		capacity = s;
+	}
+	int getSize()
+	{
+		return size;
+	}
+	bool isEmpty()
+	{
+		return size == 0;
+	}
+	// insert an element
+	void enqueue(T element)
+	{
+		if (size == capacity)
+		{
+			cout << "Queue is Full\n";
+			return;
+		}
+
+		data[nextIndex] = element;
+		nextIndex = (nextIndex + 1) % capacity;
+		if (firstIndex == -1)
+		{
+			firstIndex = 0;
+		}
+		size++;
+	}
+	T front()
+	{
+		if (isEmpty())
+		{
+			cout << "Queue is empty: \n";
+			return 0;
+		}
+		return data[firstIndex];
+	}
+	T dequeue()
+	{
+		if (isEmpty())
+		{
+			cout << "Queue is empty: \n";
+			return 0;
+		}
+		T ans = data[firstIndex];
+		firstIndex = (firstIndex + 1) % capacity;
+		size--;
+		if (size == 0)
+		{
+			firstIndex = -1;
+			nextIndex = 0;
+		}
+		return ans;
+	}
+};
---
+++ Queue/inbuildQueue.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    queue<int> q;
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    q.push(40);
+    q.push(50);
+    q.push(60);
+    cout << q.front() << endl;
+    q.pop();
+    cout << q.front() << endl;
+    cout << q.size() << endl;
+    cout << q.empty() << endl;
+    while (!q.empty())
+    {
+        cout << q.front() << endl;
+        q.pop();
+    }
+    return 0;
+}
