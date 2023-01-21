f341a804872f38cf503239d30d5c4087bb5f0d02
Faraz-mobin17 <faraz.mobin17@gmail.com>
2023-01-22 02:46:26 +0530

    added LinkedList java code

---
+++ LinkedList/java/CheckPalindrome.java
@@ -0,0 +1,21 @@
+package LinkedList.java;
+
+class Node {
+    int data;
+    Node next;
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+
+public class CheckPalindrome {
+    public static boolean isPalindrome(Node head) {
+        if (head == null)
+    }
+public static void main(String[] args) {
+    
+}    
+
+}
