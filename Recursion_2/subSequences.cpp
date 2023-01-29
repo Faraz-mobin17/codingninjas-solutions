7e82447064d84f25a89d9332dc3e8669acde5138
Faraz-mobin17 <faraz.mobin17@gmail.com>
2023-01-29 16:26:43 +0530

    added comments in subsequence program

--- Recursion_2/subSequences.cpp
+++ Recursion_2/subSequences.cpp
@@ -1,22 +1,34 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
+// input string abc
 int subs(string input, string output[]) {
-    // code here
+    // base case
     if (input.empty()) {
         output[0] = "";
         return 1;
     }
+    // call recursion on bc
     string smallString = input.substr(1);
-    int smallOutputSize = subs(smallString, output);
-    for (int i = 0; i < smallOutputSize; ++i) {
+    int smallOutputSize = subs(smallString,output);
+    /*
+        now the small output we have is 
+        ""
+        b
+        c
+        bc
+        now we need to append a in every output
+    */
+   for (int i = 0; i < smallOutputSize; ++i) {
         output[i + smallOutputSize] = input[0] + output[i];
-    }
-    return (2*smallOutputSize);
+   }
+   return (2 * smallOutputSize);
 }
 int main(int argc, char const *argv[])
 {
     string input;
     cin >> input;
+    int len = input.length();
     string* output = new string[1000];
     int count = subs(input,output);
     for (int i = 0; i < count; ++i) {
