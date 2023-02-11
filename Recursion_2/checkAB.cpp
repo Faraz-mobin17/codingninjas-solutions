4f019c87d502cd5f80942e298ecd57428bb2f97e
Faraz-mobin17 <faraz.mobin17@gmail.com>
2023-02-11 23:54:02 +0530

    improved readability

--- Recursion_2/checkAB.cpp
+++ Recursion_2/checkAB.cpp
@@ -5,12 +5,8 @@ bool checkAB(char input[]) {
 	// Write your code here
   	if (input[0] != 'a') return false;
 	if (input[0] == 'a' && input[1] == '\0') return true;
-	if (input[0] == 'a' && input[1] == 'a') {
-		return checkAB(input + 1);
-	}
-	if (input[1] == 'b' && input[2] == 'b' && input[3] == 'a') {
-		return true;
-	}
+	if (input[0] == 'a' && input[1] == 'a') return checkAB(input + 1);
+	if (input[1] == 'b' && input[2] == 'b' && input[3] == 'a') return true;
 	return checkAB(input + 1);
 }
 
