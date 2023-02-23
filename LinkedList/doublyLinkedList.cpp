2287dabe69bc390dad7906d5c8250fa307bad2c9
Faraz-mobin17 <faraz.mobin17@gmail.com>
2023-02-23 15:22:13 +0530

    added insertAtHead, insertAtTail, insertAtPos and deletePosition function

--- LinkedList/doublyLinkedList.cpp
+++ LinkedList/doublyLinkedList.cpp
@@ -2,59 +2,145 @@
 #include "Doubly.h"
 using namespace std;
 
-Node* insertAtFirst(Node* head, int data) 
+void insertAtHead(Node* &head,Node* &tail, int data) 
 {
-    Node* newNode = new Node(data);
-    if (head == NULL) { 
-        newNode->prev = NULL;
-        newNode->next = head;   
+    Node* node = new Node(data);
+    if (head == NULL) 
+    {
+        head = tail = node;
     }
-    return head;
+    else 
+    {    
+        node -> next = head;
+        node -> prev = head;
+        head = node;
+    }
 }
 
-void printNode(Node* head) {
+void insertAtTail(Node* &head,Node* &tail, int data) 
+{
+    Node* node = new Node(data);
+    if (tail == NULL) 
+    {
+         head = tail = node;
+    }
+    else 
+    {       
+        tail -> next = node;
+        node -> prev = tail;
+        tail = node;
+    }
+}
+void insertAtPos(Node* &head,Node* &tail,int position,int data) 
+{
+    
+    // inserting node at first position
+    if (position == 1) 
+    {
+        insertAtHead(head,tail,data);
+        return;
+    }
+
+    Node* node = head;
+    int count = 1;
+    // insert at specific position
+    while (node != NULL && count < position - 1) 
+    {
+        node = node -> next;
+        count++;
+    }
+    // insert at end
+    if (node -> next == NULL) 
+    {
+        insertAtTail(head,tail,data);
+        return;
+    }
+
+    Node* newNode = new Node(data);
+    newNode -> next = node -> next;
+    node -> next = newNode; 
+    node -> next -> prev = newNode;
+    newNode -> prev = node;
+}
+
+void deleteNode(Node* &head, int position) 
+{
+    // to delete first node or start node
+    if (position == 1) 
+    {
+        Node* temp = head;
+        temp -> next -> prev = NULL;
+        head = temp -> next;
+        temp -> next = NULL;
+        delete temp;
+    }
+    else 
+    {
+        // delete some other node in the list 
+        Node* current = head;
+        Node* prev = NULL;
+        int count = 1;
+        while (count < position)
+        {
+            prev = current;
+            current = current -> next;
+            count++;
+        }
+        current -> prev = NULL;
+        prev -> next = current -> next;
+        current -> next = NULL;
+        delete current;
+    }
+}
+
+void printNode(Node* head) 
+{
     Node* temp = head;
-    while (temp != NULL) {
-        cout << "Data: " << temp->data << "\n";
+    while (temp != NULL) 
+    {
+        cout  << temp->data << " ";
         temp = temp -> next;
     }
+    cout << endl;
 }
 
-int getLength(Node* head){ 
+int getLength(Node* head)
+{
     int count = 0;
     Node* temp = head;
-    while (temp != NULL) {
+    while (temp != NULL) 
+    {
         temp = temp -> next;
         ++count;
     }
     return count;
 }
 
-void insertAtFirst(Node* &head, int data) 
-{
-    Node* newNode = new Node(data);
-    newNode -> next = head;
-    head -> prev = newNode;
-    head = newNode; 
-}
-
-void insertAtTail(Node* &tail, int data) {
-    Node* newNode = new Node(data);
-    tail -> next = newNode;
-    newNode -> prev = tail;
-    tail = newNode;
-}
-
 int main(int argc, char const *argv[])
 {
-    Node* firstNode = new Node(10);
-    Node* secondNode = new Node(20);
-    Node* thirdNode = new Node(30);
-    Node* fourthNode = new Node(40);
-    Node* fifthNode = new Node(50);
-    Node* head = firstNode;
-    Node* tail = firstNode;
+    // Node* node = new Node(0);
+    // Node* head = node;
+    // Node* tail = node;
+    Node* head = NULL;
+    Node* tail = NULL;
     printNode(head);
-    cout << getLength(head);
+
+    insertAtHead(head,tail,11);
+    printNode(head);
+
+    insertAtHead(head,tail,13);
+    printNode(head);
+
+    insertAtHead(head,tail,8);
+    printNode(head);
+
+    insertAtHead(head,tail,25);
+    printNode(head);
+
+    insertAtPos(head,tail,2,100);
+    printNode(head);
+
+
+    cout << endl;
     return 0;
 }
