0919a19f1530531d59c76ece3a214ded1da7c6a4
Faraz-mobin17 <faraz.mobin17@gmail.com>
2023-03-05 02:38:38 +0530

    changed null to nullptr

--- LinkedList/Doubly.h
+++ LinkedList/Doubly.h
@@ -1,27 +1,31 @@
-class Node {
-    public:
+
+class Node
+{
+public:
     int data;
-    Node* next;
-    Node* prev;
-    Node(int data) {
-        this -> data = data;
-        this -> next = NULL;
-        this -> prev = NULL;
+    Node *next;
+    Node *prev;
+    Node(int data)
+    {
+        this->data = data;
+        this->next = nullptr;
+        this->prev = nullptr;
     }
-    Node(int data, Node* next, Node* prev) {
-        this -> data = data;
-        this -> next = next;
-        this -> prev = prev;
-     }
+    Node(int data, Node *next, Node *prev)
+    {
+        this->data = data;
+        this->next = next;
+        this->prev = prev;
+    }
     //  creating distructor
     ~Node()
     {
-        int value = this -> data;
-        if (next != NULL) 
+        int value = this->data;
+        if (next != nullptr)
         {
             delete next;
-            next = NULL;
+            next = nullptr;
         }
-        cout << "memory free for node with data: " << value << "\n";
+        std::cout << "memory free for node with data: " << value << "\n";
     }
 };
