826265901ede8316ce5aad052e2a20f9a737834f
Faraz-mobin17 <faraz.mobin17@gmail.com>
2023-03-04 23:46:51 +0530

    to check obj value in memory

---
+++ LinkedList/java/Student.java
@@ -0,0 +1,15 @@
+package LinkedList.java;
+
+public class Student {
+    public static void main(String[] args) {
+        Main s = new Main();
+        System.out.println(s);
+    }
+}
+
+class Main 
+{
+    int roll;
+    String name;
+
+}
