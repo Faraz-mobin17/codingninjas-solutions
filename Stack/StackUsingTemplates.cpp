5bfe7d9a117eb77e32603719f826fe1574025d46
Faraz-mobin17 <faraz.mobin17@gmail.com>
2023-03-10 00:38:00 +0530

    stack using template

---
+++ Stack/StackUsingTemplates.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "StackUsingTemplate.h"
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    StackUsingTemplate<char> s1;
+    s1.push(100);
+    s1.push(101);
+    s1.push(102);
+    s1.push(103);
+    s1.push(104);
+
+    cout << "Element at the top is: " << s1.top() << endl;
+    cout << "Size of the Stack: " << s1.size() << endl;
+    cout << "Element deleted: " << s1.pop() << endl;
+    cout << "Size of the Stack: " << s1.size() << endl;
+    cout << s1.isEmpty() << endl;
+
+    return 0;
+}
