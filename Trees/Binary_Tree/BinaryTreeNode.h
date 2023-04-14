b00839a62adb6a59bf8d50af7af86a2e2c331a0e
Faraz-mobin17 <faraz.mobin17@gmail.com>
2023-04-14 15:06:41 +0530

    ✅ Added params Constrcutor and Default

--- Trees/Binary_Tree/BinaryTreeNode.h
+++ Trees/Binary_Tree/BinaryTreeNode.h
@@ -5,11 +5,20 @@ public:
     int data;
     Node *left;
     Node *right;
+    Node()
+    {
+    }
     Node(int data)
     {
         this->data = data;
         this->left = nullptr;
         this->right = nullptr;
+    }
+    Node(int data, Node *left, Node *right)
+    {
+        this->data = data;
+        this->left = left;
+        this->right = right;
     }
     ~Node()
     {
