cb5a6906ccaf072f239b14883b35973b1a872e11
Faraz-mobin17 <faraz.mobin17@gmail.com>
2023-04-20 00:00:38 +0530

    ✅ minor changes

--- Recursion_2/towerOfHanoi.cpp
+++ Recursion_2/towerOfHanoi.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 using namespace std;
-void towerOfHanoi(int n, char source, char auxiliary, char destination)
+void towerOfHanoi(int n, char source, char helper, char destination)
 {
     // Write your code here
     if (n == 0)
         return;
     // recursive call
-    towerOfHanoi(n - 1, source, destination, auxiliary);
-    cout << source << " " << destination << endl;
-    towerOfHanoi(n - 1, auxiliary, source, destination);
+    towerOfHanoi(n - 1, source, destination, helper); // soruce se helper pe dalega uisng destinatino
+    // cout << source << " " << destination << endl;
+    cout << "Move disk " << n << " from rod " << source
+         << " to rod " << destination << endl;
+    towerOfHanoi(n - 1, helper, source, destination); // helper pe destinatino using source
 }
 int main()
 {
