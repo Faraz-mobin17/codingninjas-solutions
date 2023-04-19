c73252920fe12d1fcbaef2b8bc5cebf5e04dd24d
Faraz-mobin17 <faraz.mobin17@gmail.com>
2023-04-20 00:22:55 +0530

    ✅ Height of BT

--- Trees/Binary_Tree/Height.cpp
+++ Trees/Binary_Tree/Height.cpp
@@ -1,8 +1,9 @@
 #include <bits/stdc++.h>
-#include "Node.h"
+#include "BinaryTreeNode.h"
+
 using namespace std;
 
-int height(Node* root) 
+int height(Node *root)
 {
     if (root == nullptr)
     {
@@ -10,17 +11,16 @@ int height(Node* root)
     }
     int leftHeight = height(root->left);
     int rightHeight = height(root->right);
-    return 1 + max(leftHeight,rightHeight);
+    return 1 + max(leftHeight, rightHeight);
 }
-
 
 int main(int argc, char const *argv[])
 {
-    Node* root_node = new Node(150);  
-    root_node->left = new Node(250);  
-    root_node->right = new Node(270);  
-    root_node->left->left = new Node(320);  
-    root_node->left->right = new Node(350);  
+    Node *root_node = new Node(150);
+    root_node->left = new Node(250);
+    root_node->right = new Node(270);
+    root_node->left->left = new Node(320);
+    root_node->left->right = new Node(350);
     int ans = height(root_node);
     cout << ans << endl;
     return 0;
